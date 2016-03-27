8fa5a26c11a88c92a1d9232d5b3a124a7db2033b
franaragm <franaragonmesa@gmail.com>
2016-03-27 19:58:36 +0200

    - Creación de vista para mostrar Oferta en detalle - Cambios en vistas con includes de partes comunes a portada y oferta en detalle - Cambios en routing.yml, cotrolador default de OfertaBundle, repositorio de la entidad Oferta para cargar datos y vista de oferta en detalle y ofertas relacionadas. - Mostrar Ofertas relacionadas en vista usando un include sidebarwidgets - Actualizacion de css

--- src/Cupon/OfertaBundle/Controller/DefaultController.php
+++ src/Cupon/OfertaBundle/Controller/DefaultController.php
@@ -20,12 +20,35 @@ class DefaultController extends Controller
 
         $em = $this->getDoctrine()->getManager();
         $oferta = $em->getRepository('OfertaBundle:Oferta')->findOfertaDelDia($ciudad);
+        $relacionadas = $em->getRepository('OfertaBundle:Oferta')->findRelacionadas($ciudad);
 
         if (!$oferta) {
             throw $this->createNotFoundException('No se ha encontrado ninguna oferta del día');
         }
 
-        return $this->render(':frontend:portada.html.twig', array('oferta' => $oferta));
+        return $this->render(':frontend:portada.html.twig', array(
+            'oferta' => $oferta,
+            'relacionadas' => $relacionadas
+        ));
 
+    }
+
+    /**
+     * Muestra la página de detalle de la oferta indicada
+     *
+     * @param string $ciudad El slug de la ciudad a la que pertenece la oferta
+     * @param string $slug El slug de la oferta (el mismo slug se puede dar en dos o más ciudades diferentes)
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function ofertaAction($ciudad, $slug)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $oferta = $em->getRepository('OfertaBundle:Oferta')->findOferta($ciudad, $slug);
+        $relacionadas = $em->getRepository('OfertaBundle:Oferta')->findRelacionadas($ciudad);
+
+        return $this->render(':frontend:detalle.html.twig', array(
+            'oferta'   => $oferta,
+            'relacionadas' => $relacionadas
+        ));
     }
 }
--- src/Cupon/OfertaBundle/Entity/OfertaRepository.php
+++ src/Cupon/OfertaBundle/Entity/OfertaRepository.php
@@ -11,11 +11,32 @@ use Doctrine\ORM\EntityRepository;
 class OfertaRepository extends EntityRepository
 {
     /**
+     * Encuentra la oferta cuyo slug y ciudad coinciden con los indicados
+     *
+     * @param string $ciudad El slug de la ciudad
+     * @param string $slug El slug de la oferta
+     * @return mixed
+     */
+    public function findOferta($ciudad, $slug)
+    {
+        $em = $this->getEntityManager();
+        $consulta = $em->createQuery('
+            SELECT o, c, t
+            FROM OfertaBundle:Oferta o JOIN o.ciudad c JOIN o.tienda t
+            WHERE o.revisada = true AND o.slug = :slug AND c.slug = :ciudad
+        ');
+        $consulta->setParameter('slug', $slug);
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setMaxResults(1);
+        return $consulta->getOneOrNullResult();
+    }
+
+    /**
      * Encuentra la oferta del día en la ciudad indicada
      *
      * @param string $ciudad El slug de la ciudad
-     *
-     * @return object
+     * @return mixed
+     * @throws \Doctrine\ORM\NonUniqueResultException
      */
     public function findOfertaDelDia($ciudad)
     {
@@ -42,8 +63,33 @@ class OfertaRepository extends EntityRepository
         $consulta->setParameter('fecha', $date);
         $consulta->setMaxResults(1);
 
-        return $consulta->getSingleResult();
+        return $consulta->getOneOrNullResult();
 
+    }
+
+    /**
+     * Encuentra las cinco ofertas más cercanas a la ciudad indicada
+     *
+     * @param string $ciudad El slug de la ciudad
+     * @return array
+     */
+    public function findRelacionadas($ciudad)
+    {
+        $em = $this->getEntityManager();
+        $consulta = $em->createQuery('
+            SELECT o, c
+            FROM OfertaBundle:Oferta o
+            JOIN o.ciudad c
+            WHERE o.revisada = true
+            AND o.fechaPublicacion <= :fecha
+            AND c.slug != :ciudad
+            ORDER BY o.fechaPublicacion DESC
+        ');
+        $consulta->setMaxResults(5);
+        $consulta->setParameter('ciudad', $ciudad);
+        $consulta->setParameter('fecha', new \DateTime('today'));
+
+        return $consulta->getResult();
     }
 
 }
