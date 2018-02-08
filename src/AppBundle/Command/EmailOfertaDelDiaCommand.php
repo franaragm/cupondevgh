a1ce143460b67b89867758d5dddfe397f0c91e4b
Fran Aragon Mesa <franaragonmesa@gmail.com>
2018-02-08 11:29:30 +0100

    fix command email

--- src/AppBundle/Command/EmailOfertaDelDiaCommand.php
+++ src/AppBundle/Command/EmailOfertaDelDiaCommand.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\Console\Input\InputOption;
 
 /**
  * Comando que envía cada día un email a todos los usuarios que lo
